c0cb62f4bce3f4a17608c3d785ff486a6293320c
lixutang <284977653@qq.com>
2016-06-02 09:56:50 +0800

    11a

--- app/main/views.py
+++ app/main/views.py
@@ -1,15 +1,22 @@
 from flask import render_template, redirect, url_for, abort, flash
 from flask.ext.login import login_required, current_user
 from . import main
-from .forms import EditProfileForm, EditProfileAdminForm
+from .forms import EditProfileForm, EditProfileAdminForm,PostForm
 from .. import db
-from ..models import Role, User
-from ..decorators import admin_required
+from ..models import Role, User,Post
+from ..decorators import admin_required,Permission
 
 
-@main.route('/')
+@main.route('/', methods =['GET','POST'])
 def index():
-    return render_template('index.html')
+    form = PostForm()
+    if current_user.can(Permission.WRITE_ARTICLES) and \
+            form.validate_on_submit():
+        post = Post(body = form.body.data,author=current_user._get_current_object())
+        db.session.add(post)
+        return redirect(url_for('.index'))
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html',form=form,posts=posts)
 
 
 @main.route('/user/<username>')
