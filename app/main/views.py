4585b355599d3965f72187097d8d848acbe18b7e
lixutang <cuclixutang@163.com>
2016-06-07 14:58:07 +0800

    11.3

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,5 @@
-from flask import render_template, redirect, url_for, abort, flash
+from flask import render_template, redirect, url_for, abort, flash, request,\
+        current_app
 from flask.ext.login import login_required, current_user
 from . import main
 from .forms import EditProfileForm, EditProfileAdminForm,PostForm
@@ -14,9 +15,12 @@ def index():
             form.validate_on_submit():
         post = Post(body = form.body.data,author=current_user._get_current_object())
         db.session.add(post)
-        return redirect(url_for('.index'))
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('index.html',form=form,posts=posts)
+        return redirect(url_for('.index')) 
+    page =request.args.get('page',1,type = int)
+    pagination =Post.query.order_by(Post.timestamp.desc()).paginate(
+        page,per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],error_out=False)
+    posts = pagination.items
+    return render_template('index.html',form=form,posts=posts,pagination=pagination)
 
 
 @main.route('/user/<username>')
