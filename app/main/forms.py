1b7ff3d640a2d43f0e08bce5b281cdfbd2b9eb36
lixutang <cuclixutang@163.com>
2016-06-14 23:29:50 +0800

    13b

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,11 +3,9 @@ from wtforms import StringField, TextAreaField, BooleanField, SelectField,\
     SubmitField
 from wtforms.validators import Required, Length, Email, Regexp
 from wtforms import ValidationError
+from flask.ext.pagedown.fields import PageDownField
 from ..models import Role, User
 
-class PostForm(Form):
-    body = TextAreaField("what on your mind",validators=[Required()])
-    submit = SubmitField('Submit')
 
 class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
@@ -50,3 +48,13 @@ class EditProfileAdminForm(Form):
         if field.data != self.user.username and \
                 User.query.filter_by(username=field.data).first():
             raise ValidationError('Username already in use.')
+
+
+class PostForm(Form):
+    body = PageDownField("What's on your mind?", validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class CommentForm(Form):
+    body = StringField('Enter your comment', validators=[Required()])
+    submit = SubmitField('Submit')
